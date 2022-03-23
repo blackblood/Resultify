1a38620e232950d1cab56dc8b49f8d00072fac04
Akshay Takkar <akshayt@m3india.in>
2022-03-24 01:41:53 +0530

    added optionify method

--- lib/resultify.rb
+++ lib/resultify.rb
@@ -4,6 +4,7 @@ module Resultify
   class Result
     attr_accessor :value, :err
     attr_reader :error_handler
+    attr_reader :value_handler
 
     def initialize(ret_val, err)
       @value = ret_val
@@ -17,17 +18,41 @@ module Resultify
       end
     end
 
-    def value
-      if self.error_handler.is_a?(Proc)
-        @value
-      else
-        raise "Define the error handler on this result object first"
+    def value_handler=(f)
+      raise "Define error_handle" if !@error_handler.is_a?(Proc)
+      @result_handler = f
+      if @err == nil
+        f.call(@value)
+      end
+    end
+  end
+
+  class Option
+    attr_accessor :value
+    attr_reader :blank_handler
+    attr_reader :value_handler
+
+    def initialize(ret_val)
+      @value = ret_val
+    end
+
+    def blank_handler=(f)
+      @blank_handler = f
+      if @value == nil || @value == ""
+        f.call
+      end
+    end
+
+    def value_handler=(f)
+      raise "Define blank_handle" if !@blank_handler.is_a?(Proc)
+      if @value != nil && @value != ""
+        f.call(@value)
       end
     end
   end
 
   module ClassMethods
-    def overwrite_method(name)
+    def overwrite_method_for_result(name)
       @@overwriting_method = true
       alias_method "old_#{name}".to_sym, name.to_sym
       define_method name do |*args, &block|
@@ -42,18 +67,44 @@ module Resultify
       @@overwriting_method = false
     end
 
+    def overwrite_method_for_option(name)
+      @@overwriting_method = true
+      alias_method "old_#{name}".to_sym, name.to_sym
+      define_method name do |*args, &block|
+        old_method = self.class.instance_method("old_#{name}")
+        ret_val = old_method.bind(self).call(*args, &block)
+        if ret_val == nil || ret_val == ""
+          return Option.new(ret_val)
+        else
+          return Option.new(nil)
+        end
+      end
+      @@overwriting_method = false
+    end
+
     def resultify(*method_names)
-      @@method_names = method_names
+      @@resultify_method_names = method_names
       self.instance_methods(false).each do |mname|
-        self.overwrite_method(mname) if @@method_names.include?(mname)
+        self.overwrite_method_for_result(mname) if @@resultify_method_names.include?(mname)
+      end
+      @@overwriting_method = false
+    end
+
+    def optionify(*method_names)
+      @@optionify_method_names = method_names
+      self.instance_methods(false).each do |mname|
+        self.overwrite_method_for_option(mname) if @@optionify_method_names.include?(mname)
       end
       @@overwriting_method = false
     end
 
     def method_added(name)
-      if defined?(@@method_names) && @@method_names.include?(name)
-        return if defined?(@@overwriting_method) && @@overwriting_method
-        self.overwrite_method(name)
+      return if defined?(@@overwriting_method) && @@overwriting_method
+      if defined?(@@resultify_method_names) && @@resultify_method_names.include?(name)
+        self.overwrite_method_for_result(name)
+      end
+      if defined?(@@optionify_method_names) && @@optionify_method_names.include?(name)
+        self.overwrite_method_for_option(name)
       end
     end
   end
